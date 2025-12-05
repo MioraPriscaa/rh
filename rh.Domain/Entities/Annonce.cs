8b357bee35a4e19e49531ef343f82fea681b0397
priscamiora <119618862+MioraPriscaa@users.noreply.github.com>
2025-12-05 20:52:33 +0300

    annonce

---
+++ rh.Domain/Entities/Annonce.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace rh.Domain.Entities
+{
+    public class Annonces
+    {
+        public int id { get; set; }
+
+        [Required]
+        public string nom { get; set; }
+
+        public DateTime date { get; set; }
+    }
+}
---
+++ rh.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using rh.Domain.Entities;
+namespace rh.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Annonce> Annonces { get; set; }
+}
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            // Remplace la chaîne de connexion par la tienne
+            optionsBuilder.UseSqlServer("Server=.;Database=rh_db;Trusted_Connection=True;");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
