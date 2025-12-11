f01cd8f51714c98e664262ddc3e5f825ce296ca7
priscamiora <miora.a@generis.mg>
2025-12-11 15:45:18 +0300

    correction

---
+++ rh.BackOffice/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿namespace rh.BackOffice.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    namespace rh.BackOffice.Controllers
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class TestController : ControllerBase
+        {
+            [HttpGet("ping")]
+            public IActionResult Ping()
+            {
+                return Ok(new { message = "pong" });
+            }
+        }
+    }
+
+}
--- rh.BackOffice/Program.cs
+++ rh.BackOffice/Program.cs
@@ -1,12 +1,45 @@
-using rh.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+using rh.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// DB Context
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-// OU Npgsql si PostgreSQL
+
+// Ajouter Razor Pages
+builder.Services.AddRazorPages();
+
+// Ajouter Controllers (API)
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API RH", Version = "v1" });
+});
 
 var app = builder.Build();
+
+// Middleware
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Routing
+app.MapControllers();
+
+// Razor Pages
+app.MapRazorPages();
+
+// Optionnel : fallback pour tout le reste (SPA ou URLs inconnues)
+app.MapFallbackToPage("/Index");
 
 app.Run();
